8a8081f48a9d54ad1e2eda9400646044e5961dba
LeonLipu <lipusipupara@gmail.com>
2017-07-13 00:43:08 +0530

    removed error on workspace

--- flow.groovy
+++ flow.groovy
@@ -3,6 +3,8 @@ def pipeline(Map<String,String> dataSet){
 
   def jobs=load("jobRepo.groovy");
 
+  ws("/Users/brahmanandakar/Desktop/jenkin/gitrepo1") {
+
   stage('checkout'){
       jobs.checkout(dataSet)
       }
@@ -13,7 +15,7 @@ def pipeline(Map<String,String> dataSet){
   stage('Selenium_Automation'){
           jobs.selenium(dataSet)
         }
-
+   }
 
 }
 
--- jobRepo.groovy
+++ jobRepo.groovy
@@ -1,43 +1,20 @@
 
 def checkout(Map<String,String> dataSet){
-
-
-
-  ws("/Users/brahmanandakar/Desktop/jenkin/gitrepo1") {
-
     checkout([$class: 'GitSCM',
     branches: [[name: dataSet.get('seleniumBranch')]],
     extensions: [[$class: 'CleanCheckout']],
     userRemoteConfigs: [[credentialsId: dataSet.get('git-id'), url: dataSet.get('seleniumRepoUrl')]]]);
 
-  }
-
-
-
 }
 
 def build(Map<String,String> dataSet){
 
-  ws("/Users/brahmanandakar/Desktop/jenkin/gitrepo1") {
-
     sh 'mvn install -DskipTests'
-
-  }
-
-
 
 }
 
 def selenium(Map<String,String> dataSet){
-
-  ws("/Users/brahmanandakar/Desktop/jenkin/gitrepo1") {
-
     sh 'mvn test'
-
-  }
-
-
-
 }
 
 return this;
