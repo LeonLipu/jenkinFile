2170188fe785875df838ee580261e3d53506335a
LeonLipu <lipusipupara@gmail.com>
2017-07-13 03:18:26 +0530

    modified workspace and kept old file

--- flow.groovy
+++ flow.groovy
@@ -18,12 +18,14 @@ def pipeline(Map<String,String> dataSet){
   stage('BuildGeneration'){
 
            sh 'mvn install -DskipTests'
+           junit
 
           }
 
   stage('Selenium_Automation'){
 
       sh 'mvn test'
+      junit '**/target/surefire-reports/TEST-*.xml'
 
         }
    }
